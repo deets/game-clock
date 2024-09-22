9543753ab8448911a605448789414450238f8046
Diez B. Roggisch <deets@web.de>
2024-09-22 15:23:33 +0200

    Move ui files into main project

---
+++ sim/.dir-locals.el
@@ -0,0 +1,6 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((projectile-project-root . "~/projects/private/game-clock/sim/")))
+ (c-mode . ((projectile-project-run-cmd . nil)))
+ (cmake-mode . ((projectile-project-run-cmd . nil))))
